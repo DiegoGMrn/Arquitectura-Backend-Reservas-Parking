3ca5d9bcbae36074203862e04024b7f72352afb7
Zheol <marcelo.jimenez@alumnos.ucn.cl>
2024-06-24 20:40:09 -0400

    conexion kubernetes

--- src/common/proxy/notifications-proxy.ts
+++ src/common/proxy/notifications-proxy.ts
@@ -10,7 +10,7 @@ export const clientProxyNotifications = (): ClientGrpcProxy => {
     return ClientProxyFactory.create({
         transport: Transport.GRPC,
         options: {
-            url: '0.0.0.0:50054',
+            url: process.env.NOTIFICATIONS_GRPC_SERVER_URL || '0.0.0.0:50054',
             package: 'notifications',
             protoPath: join(__dirname, '../../../src/bookings/emails.proto'),
         },
